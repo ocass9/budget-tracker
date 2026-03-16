db077b71c520d03206c9d5411cf53caebb1ddb45
otis cassagne <u7627045@anu.edu.au>
2026-03-16 11:22:27 +1100

    commit 3

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask
-from models import db
+from flask import Flask, render_template, request, redirect, url_for
+from models import db, Transaction
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///budget.db"
@@ -10,6 +11,21 @@ db.init_app(app)
 @app.route("/")
 def index():
     return "Hello Flask"
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        transaction = Transaction(
+            amount=float(request.form["amount"]),
+            type=request.form["type"],
+            category=request.form["category"],
+            date=datetime.strptime(request.form["date"], "%Y-%m-%d").date(),
+            note=request.form.get("note","")
+        )
+        db.session.add(transaction)
+        db.session.commit()
+        return redirect(url_for("index"))
+    return render_template("add.html")
 
 if __name__ == "__main__":
     with app.app_context():
